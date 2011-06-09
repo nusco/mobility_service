685212257821154cf5ac556d373f252ecee852a0
Paolo "Nusco" Perrotta <paolo.nusco.perrotta@gmail.com>
2011-06-09 16:10:30 +0200

    merge di Andrea

--- gen/mobi/mism/schemas/messages/mobility/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://schemas.mism.mobi/Messages/Mobility", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package mobi.mism.schemas.messages.mobility;
--- gen/mobi/mism/schemas/messages/notification/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://schemas.mism.mobi/Messages/Notification", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package mobi.mism.schemas.messages.notification;
